73c18ae26ec5be47f1ce8dc5f685e1bd7a501e89
faris-trinitywizards <faris.g@trinitywizards.com>
2023-12-16 21:55:11 +0800

    Register on init

--- app/src/test/java/com/fghilmany/gofoodclone/register/presentation/RegisterViewModelTest.kt
+++ app/src/test/java/com/fghilmany/gofoodclone/register/presentation/RegisterViewModelTest.kt
@@ -4,8 +4,15 @@ import com.fghilmany.register.domain.RegisterBody
 import com.fghilmany.register.domain.RegisterInsert
 import io.mockk.clearAllMocks
 import io.mockk.confirmVerified
+import io.mockk.every
 import io.mockk.spyk
 import io.mockk.verify
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -15,9 +22,12 @@ class RegisterViewModelTest{
     private val useCase = spyk<RegisterInsert>()
     private lateinit var sut: RegisterViewModel
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Before
     fun setUp(){
         sut = RegisterViewModel(useCase)
+
+        Dispatchers.setMain(UnconfinedTestDispatcher())
     }
 
     @After
@@ -38,6 +48,41 @@ class RegisterViewModelTest{
             "acuy@email.com",
         )
         verify(exactly = 0) {
+            useCase.register(body)
+        }
+
+        confirmVerified(useCase)
+    }
+
+    @Test
+    fun testRegisterData() = runBlocking {
+        val body = RegisterBody(
+            "123",
+            "123",
+            "Bandung",
+            "082134",
+            "Bandung",
+            "Acuy",
+            "17",
+            "acuy@email.com",
+        )
+        every {
+            useCase.register(body)
+        } returns flowOf()
+
+        sut.setRegisterBody(
+            "123",
+            "123",
+            "Bandung",
+            "082134",
+            "Bandung",
+            "Acuy",
+            "17",
+            "acuy@email.com",
+        )
+        sut.register()
+
+        verify(exactly = 1) {
             useCase.register(body)
         }
 
