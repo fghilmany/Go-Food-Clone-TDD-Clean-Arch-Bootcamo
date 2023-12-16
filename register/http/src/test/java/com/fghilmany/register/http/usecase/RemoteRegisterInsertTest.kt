536ced735946e039a83b030f75857b4770de2e96
faris-trinitywizards <faris.g@trinitywizards.com>
2023-12-16 15:25:34 +0800

    Request data once on load

--- register/http/src/test/java/com/fghilmany/register/http/usecase/RemoteRegisterInsertTest.kt
+++ register/http/src/test/java/com/fghilmany/register/http/usecase/RemoteRegisterInsertTest.kt
@@ -1,10 +1,15 @@
 package com.fghilmany.register.http.usecase
 
+import app.cash.turbine.test
+import com.fghilmany.register.domain.RegisterBody
 import com.fghilmany.register.http.RegisterHttpClient
 import com.fghilmany.register.http.RemoteRegisterBody
 import io.mockk.confirmVerified
+import io.mockk.every
 import io.mockk.spyk
 import io.mockk.verify
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 
@@ -31,6 +36,44 @@ class RemoteRegisterInsertTest{
         )
         verify(exactly = 0) {
             client.register(body)
+        }
+
+        confirmVerified(client)
+    }
+
+    @Test
+    fun testLoadRequestsData() = runBlocking{
+        val remoteBody = RemoteRegisterBody(
+            "123",
+            "123",
+            "Bandung",
+            "082134",
+            "Bandung",
+            "Acuy",
+            "17",
+            "acuy@email.com",
+        )
+        val body = RegisterBody(
+            "123",
+            "123",
+            "Bandung",
+            "082134",
+            "Bandung",
+            "Acuy",
+            "17",
+            "acuy@email.com",
+        )
+
+        every {
+            client.register(remoteBody)
+        } returns flowOf()
+
+        sut.register(body).test{
+            awaitComplete()
+        }
+
+        verify(exactly = 1) {
+            client.register(remoteBody)
         }
 
         confirmVerified(client)
